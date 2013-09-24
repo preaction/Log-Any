704c28d9cb1c950ee0855813ab5bab5b27bfe3ab
David Golden <dagolden@cpan.org>
2013-09-24 16:24:37 -0400

    start documenting Log::Any::Proxy

--- lib/Log/Any/Proxy.pm
+++ lib/Log/Any/Proxy.pm
@@ -74,3 +74,47 @@ foreach my $name ( Log::Any->logging_methods, keys(%aliases) ) {
 
 1;
 
+=attr adapter (required)
+
+A L<Log::Any::Adapter> object to receive any messages logged.
+
+=attr filter
+
+A code reference to transform messages before passing them to a
+Log::Any::Adapter.  It takes a single string argument and should
+return a scalar.  If the return value is undef or the empty string,
+no message will be logged.  Otherwise, the return value is passed
+to the logging adapter.
+
+=attr formatter
+
+A code reference to format messages given to the C<*f> methods
+(C<tracef>, C<debugf>, C<infof>, etc..)  It takes a single
+string argument and must return a string argument.
+
+The default formatter acts like C<sprintf>, except that undef
+arguments are changed to C<< <undef> >> and any references or
+objects are dumped via L<Data::Dumper> (but without newlines).
+
+=attr prefix
+
+If defined, this string will be prepended to all messages.
+It will not include a trailing space, so add that yourself
+if you want.
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 USAGE
+
+=head2 Logging
+
+=head2 Filtering
+
+=head3 Formatting
+
+=head3 Setting a prefix
+
+=cut
+
