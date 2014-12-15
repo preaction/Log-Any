66d415e045d114cad45a47f6db25b964beb164c0
David Golden <dagolden@cpan.org>
2014-12-14 22:06:57 -0500

    clean up Base

--- lib/Log/Any/Adapter/Base.pm
+++ lib/Log/Any/Adapter/Base.pm
@@ -7,7 +7,9 @@ package Log::Any::Adapter::Base;
 our $VERSION = '0.91'; # TRIAL
 
 use Log::Any;
-use Log::Any::Adapter::Util qw/make_method/;
+
+# we import dump_one_line in case anything uses it
+use Log::Any::Adapter::Util qw/make_method dump_one_line/;
 
 sub new {
     my $class = shift;
@@ -19,6 +21,7 @@ sub new {
 
 sub init { }
 
+# we have this in case anything uses it
 sub delegate_method_to_slot {
     my ( $class, $slot, $method, $adapter_method ) = @_;
 
@@ -27,42 +30,13 @@ sub delegate_method_to_slot {
         $class );
 }
 
-sub dump_one_line {
-    my ( $self, $value ) = @_;
-
-    return Data::Dumper->new( [$value] )->Indent(0)->Sortkeys(1)->Quotekeys(0)
-      ->Terse(1)->Useqq(1)->Dump();
-}
-
 # Forward 'warn' to 'warning', 'is_warn' to 'is_warning', and so on for all aliases
-#
 my %aliases = Log::Any->log_level_aliases;
 while ( my ( $alias, $realname ) = each(%aliases) ) {
     make_method( $alias, sub { my $self = shift; $self->$realname(@_) } );
     my $is_alias    = "is_$alias";
     my $is_realname = "is_$realname";
     make_method( $is_alias, sub { my $self = shift; $self->$is_realname(@_) } );
-}
-
-# Add printf-style versions of all logging methods and aliases - e.g. errorf, debugf
-#
-foreach my $name ( Log::Any->logging_methods, keys(%aliases) ) {
-    my $methodf = $name . "f";
-    my $method = $aliases{$name} || $name;
-    make_method(
-        $methodf,
-        sub {
-            my ( $self, $format, @params ) = @_;
-            my @new_params =
-              map {
-                   !defined($_) ? '<undef>'
-                  : ref($_)     ? $self->dump_one_line($_)
-                  : $_
-              } @params;
-            my $new_message = sprintf( $format, @new_params );
-            $self->$method($new_message);
-        }
-    );
 }
 
 1;
