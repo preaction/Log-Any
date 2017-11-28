f52f4bb0ea586e2fee61454137a96da7a445183c
Doug Bell <doug@preaction.me>
2017-11-28 15:18:56 -0600

    incrementing version after release

--- lib/Log/Any/Adapter/Stderr.pm
+++ lib/Log/Any/Adapter/Stderr.pm
@@ -5,7 +5,7 @@ use warnings;
 package Log::Any::Adapter::Stderr;
 
 # ABSTRACT: Simple adapter for logging to STDERR
-our $VERSION = '1.702';
+our $VERSION = '1.703';
 
 use Log::Any::Adapter::Util ();
 
