fd3044069e0d0ba0a4cd57adc53935da0bf4e01a
Doug Bell <doug@preaction.me>
2017-12-17 18:13:52 -0600

    incrementing version after release

--- lib/Log/Any/Adapter/Syslog.pm
+++ lib/Log/Any/Adapter/Syslog.pm
@@ -5,7 +5,7 @@ use warnings;
 package Log::Any::Adapter::Syslog;
 
 # ABSTRACT: Send Log::Any logs to syslog
-our $VERSION = '1.704';
+our $VERSION = '1.705';
 
 use Log::Any::Adapter::Util qw{make_method};
 use base qw{Log::Any::Adapter::Base};
