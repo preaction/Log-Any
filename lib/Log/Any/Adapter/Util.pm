b834d05d61bc82762f9048ebe611f85cc3477aa0
Doug Bell <doug@preaction.me>
2018-01-17 13:50:01 -0600

    incrementing version after release

--- lib/Log/Any/Adapter/Util.pm
+++ lib/Log/Any/Adapter/Util.pm
@@ -5,7 +5,7 @@ use warnings;
 package Log::Any::Adapter::Util;
 
 # ABSTRACT: Common utility functions for Log::Any
-our $VERSION = '1.705';
+our $VERSION = '1.706';
 
 use Exporter;
 our @ISA = qw/Exporter/;
--- lib/Log/Any/Proxy/Test.pm
+++ lib/Log/Any/Proxy/Test.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Log::Any::Proxy::Test;
 
-our $VERSION = '1.705';
+our $VERSION = '1.706';
 
 use Log::Any::Proxy;
 our @ISA = qw/Log::Any::Proxy/;
